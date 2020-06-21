603904fb04db679e22df90b05b9e8e1afb1ac79f
unani <unani92@naver.com>
2020-06-21 15:34:39 +0900

    fin paint tool clone coding

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ canvas.width = 700
 canvas.height = 700
 
 ctx.strokeStyle = "black";
+ctx.fillStyle = ctx.strokeStyle;
 ctx.lineWidth = 2.5
 
 // control menu 
@@ -13,57 +14,86 @@ const colors = document.querySelectorAll(".jsColor")
 const input = document.querySelector("input")
 
 let painting = false; 
-
+let filling = false;
 
 function onMouseMove(event) {
-    const x = event.offsetX;
-    const y = event.offsetY;
-    // 클릭하지 않고 움직이는 경우
-    if (!painting) {
-        ctx.beginPath(); // path를 생성하고
-        ctx.moveTo(x,y); // 해당 좌표로 이동합니다.
-    } else {
-        // 마우스가 움직이는 동안 계속 발생하는 이벤트
-        ctx.lineTo(x,y); // path의 이전 위치에서 지금 위치까지의 선을 생성
-        ctx.stroke(); // 최종적으로 선을 표시해준다
+  const x = event.offsetX;
+  const y = event.offsetY;
+  // 클릭하지 않고 움직이는 경우
+  if (!painting) {
+    ctx.beginPath(); // path를 생성하고
+    ctx.moveTo(x,y); // 해당 좌표로 이동합니다.
+  } else {
+    // 마우스가 움직이는 동안 계속 발생하는 이벤트
+    ctx.lineTo(x,y); // path의 이전 위치에서 지금 위치까지의 선을 생성
+    ctx.stroke(); // 최종적으로 선을 표시해준다
     }
 }
 
 function onMouseDown(event) {
-    painting = true;
+  painting = true;
 }
 
 function stopPainting(event) {
-    painting = false;
+  painting = false;
 }
 
 function startPainting(event) {
-    painting = true;
+  painting = true;
 }
 
 function changeColor() {
-    console.log(event.target.style)
+  console.log(event.target.style)
 }
 
 if (canvas) {
-    canvas.addEventListener("mousemove", onMouseMove)
-    canvas.addEventListener("mousedown", startPainting)
-    canvas.addEventListener("mouseup", stopPainting)
-    canvas.addEventListener("mouseleave", stopPainting)
+  canvas.addEventListener("mousemove", onMouseMove)
+  canvas.addEventListener("mousedown", startPainting)
+  canvas.addEventListener("mouseup", stopPainting)
+  canvas.addEventListener("mouseleave", stopPainting)
 }
 
 // change color
 colors.forEach(color => {
-    color.addEventListener("click", event => {
-        event.target.classList.add("active")
-        ctx.strokeStyle = event.target.style.backgroundColor
-    })
+  color.addEventListener("click", event => {
+    const colorPickers  = document.querySelectorAll(".jsColor")
+    colorPickers.forEach(colorPicker => {colorPicker.classList.remove("active")})
+    event.target.classList.add("active")
+    ctx.strokeStyle = event.target.style.backgroundColor
+    ctx.fillStyle = ctx.strokeStyle
+  })
 })
 
 // change linewidth
-input.addEventListener("change",event=>{
-    ctx.lineWidth = event.target.value
+input.addEventListener("change", event=> ctx.lineWidth = event.target.value)
+
+
+// change mode paint, fill
+const mode = document.querySelector("#jsMode")
+mode.addEventListener("click", () => {
+  if (filling === true) {
+    event.target.innerText = "FILL"
+    filling = false
+  } else {
+    event.target.innerText = "Paint"
+    filling = true
+    ctx.fillStyle = ctx.strokeStyle
+  }
+})
+canvas.addEventListener("click", () => {
+  if (filling === true) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+  }
 })
 
+// save painting
+const saveBtn = document.querySelector("#jssave")
+saveBtn.addEventListener("click", () => {
+  const image = canvas.toDataURL("image/jpeg")
+  const link = document.createElement("a")
+  link.href = image
+  link.download = `paintimages`
+  link.click()
+})
 
 
